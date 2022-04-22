b2e0e95d922461619699ebae43558ccb4e0a65d7
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-22 16:52:03 -0300

    alteração no border radius conforme esperado

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <Output />
+      <Output border={border} />
       <TextValue border={border} />
       <Slider
         border={border}
--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 type BorderProps = {
-   border?: number;
+   border: number;
 }
 
-
 export default function Output(props: BorderProps) {
+   const { border } = props;
+   let text = border + '%'
+
    return (
-      <div className='output'>
+      <div className='output' style={{ borderRadius: text }}>
          Output
       </div>
    )
